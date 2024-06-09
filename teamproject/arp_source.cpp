bd69b2efbe8549155168c96cbfa05f86a83df891
beomsik0211 <149175965+beomsik0211@users.noreply.github.com>
2024-06-10 06:50:51 +0900

    Update arp_source.cpp

--- teamproject/arp_source.cpp
+++ teamproject/arp_source.cpp
@@ -1,34 +1,74 @@
 #include "ether_header.h"
 #include "ip_header.h"
 #include "arp_header.h"
+#include "arp_struct.h"
+#include "ip_struct.h"
 
 void print_arp(arp_header *arp,ip_header *ih){
 
     extern packet* pk;
     print_ip(pk->ip);
     arp_header* ah;
-    ah=pk->arp;
+    //ah=pk->arp;(
 
-    ah=(arp_header*)malloc(ntohs(ah->arp_hardtype));
-    ah=(arp_header*)(pk->app+ETHER_LENGTH+(pk))
-    printf("************************** ARP Header *****************************");
-    printf("Hardware type: %d\n",ntohs(ah->arp_hardtype));
+   // ah=(arp_header*)malloc(ntohs(ah->arp_hwtype));
+    ah = (arp_header*)(pk->app + ETHER_LENGTH + (pk->ip->ip_leng * 2));
+   // ah=(arp_header*)(pk->app+ETHER_LENGTH+(pk))
+   // printf("************************** ARP Header *****************************");
+
+
+    //printf("Hardware type: %d\n",ntohs(ah->arp_hwtype));
     printf("\n");
-    printf("Protocol:%d\n",ntohs(ah->arp_proto));
+    printf("Protocol:%d\n", ntohs(ah->arp_ptype));
     printf("\n");
-    printf("Hardsize: %d\n",ntohs(ah->arp_hardsize));
-    printf("\n");
-    printf("protosize: %d\n",ntohs(ah->arp_protosize));
+    printf("protosize: %d\n",ntohs(ah->arp_plen));
     printf("\n");
     printf("opcode: %d\n",ntohs(ah->arp_opcode));
     printf("\n");
-    printf("smacadd: %d\n",ntohs(ah->arp_smacadd));
+    printf("smacadd: %d\n",ntohs(ah->arp_shost[ETHER_ADDR_LEN]));
     printf("\n");
-    printf("sipadd: %d\n",ntohs(ah->arp_sipadd));
+   // printf("sipadd: %d\n", ntohs(ah->arp_saddr));
     printf("\n");
-    printf("tmacadd: %d\n",ntohs(ah->arp_tamcadd));
+    printf("tmacadd: %d\n",ntohs(ah->arp_dhost[ETHER_ADDR_LEN]));
+    //printf("\n");
+   // printf("tipadd: %d\n",ntohs(ah->arp_daddr));
+
+
+}
+#include "ether_header.h"
+#include "ip_header.h"
+#include "arp_header.h"
+#include "arp_struct.h"
+#include "ip_struct.h"
+
+void print_arp(arp_header *arp,ip_header *ih){
+
+    extern packet* pk;
+    print_ip(pk->ip);
+    arp_header* ah;
+    //ah=pk->arp;(
+
+   // ah=(arp_header*)malloc(ntohs(ah->arp_hwtype));
+    ah = (arp_header*)(pk->app + ETHER_LENGTH + (pk->ip->ip_leng * 2));
+   // ah=(arp_header*)(pk->app+ETHER_LENGTH+(pk))
+   // printf("************************** ARP Header *****************************");
+
+
+    //printf("Hardware type: %d\n",ntohs(ah->arp_hwtype));
     printf("\n");
-    printf("tipadd: %d\n",ntohs(ah->arp_tipadd));
+    printf("Protocol:%d\n", ntohs(ah->arp_ptype));
+    printf("\n");
+    printf("protosize: %d\n",ntohs(ah->arp_plen));
+    printf("\n");
+    printf("opcode: %d\n",ntohs(ah->arp_opcode));
+    printf("\n");
+    printf("smacadd: %d\n",ntohs(ah->arp_shost[ETHER_ADDR_LEN]));
+    printf("\n");
+   // printf("sipadd: %d\n", ntohs(ah->arp_saddr));
+    printf("\n");
+    printf("tmacadd: %d\n",ntohs(ah->arp_dhost[ETHER_ADDR_LEN]));
+    //printf("\n");
+   // printf("tipadd: %d\n",ntohs(ah->arp_daddr));
 
 
 }
