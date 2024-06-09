6b9877de2314812e4d5ae3a3e10e8d9e819f0182
beomsik0211 <149175965+beomsik0211@users.noreply.github.com>
2024-06-10 07:22:48 +0900

    Update icmp_source.cpp

--- teamproject/icmp_source.cpp
+++ teamproject/icmp_source.cpp
@@ -7,7 +7,17 @@ void print_icmp(icmp_header* arp, ip_header* ih) {
 	extern packet* pk;
 	print_ip(pk->ip);
 	icmp_header* ich;
-	ich = pk->icmp;
+	//ich = pk->icmp;
+
+
+	printf("icmp type: %d\n", ntohs(ich->icmp_type));
+	printf("\n");
+	printf("icmp code: %d\n", ntohs(ich->icmp_code));
+	printf("\n");
+	printf("icmp checksum: %d\n", ntohs(ich->icmp_checksum));
+	printf("\n");
+	printf("icmp other: %d\n", ntohs(ich->icmp_other));
+
 
 
 
